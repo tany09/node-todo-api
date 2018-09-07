3da2c3660d7c6ada2d30ed08b5f223fd84a2bb62
Tanveer Singh Dhawan <tanysingh09@gmail.com>
2018-09-07 18:39:52 +0530

    Add GET /todos/id route to find todo with id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const {User} = require('./models/user');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const {ObjectId} = require('mongodb'); 
 
 app.use(bodyParser.json());
 
@@ -33,6 +34,21 @@ app.get('/users', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectId.isValid(id)) {
+        res.status(404).send('Error: Invalid todo id');
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send('Error: Todo not found with the given id');
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.send(400).send('Some error occured');
+    });
 });
 
 app.listen(3000, () => {
