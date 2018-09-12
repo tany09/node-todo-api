c8f69695ca56878875c5b09c998336aa43c2ac0e
Tanveer Singh Dhawan <tanysingh09@gmail.com>
2018-09-13 02:01:58 +0530

    Add GET /users/me private route and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -47,8 +47,23 @@ UserSchema.methods.generateAuthToken = function () {
     return user.save().then(() => {
         return token;
     })
-
 }
+
+UserSchema.statics.findByToken = function (token) {
+    const User = this;
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, '123abc');
+    } catch (e) {
+        return Promise.reject();
+    }
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
 
 const User = mongoose.model('Users', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 const express = require('express');
 const bodyParser = require('body-parser');
+const {authenticate} = require('./middleware/authenticate');
 const app = express();
 const {ObjectId} = require('mongodb'); 
 const port = process.env.PORT;
@@ -109,6 +110,10 @@ app.post('/users', (req, res) => {
         res.status(400).send(err);
     });
 });
+
+app.get('/users/me', authenticate, (req,res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
